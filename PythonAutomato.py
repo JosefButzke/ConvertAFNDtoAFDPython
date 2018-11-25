6f09abae0c949a351523332ae5d151935bb026fb
JosefButzke <josef-stein@hotmail.com>
2018-11-25 19:57:46 -0200

    Funcao BuscarTransicao, ContruirNovasLinhas adicionadas. Imagem do automato de exemplo gerada via JFLAP.

--- PythonAutomato.py
+++ PythonAutomato.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 class Automato:
     def __init__(self, estados = [], estadoInicial = [], estadosFinais = [], transicoes = [], alfabeto = []):
@@ -25,25 +26,60 @@ def carregaDados(automato):
     automato.alfabeto = sorted(set(letrasDoArquivo))
 
 def montarTabelaAFDInicial(automato):
-    tabelaAFD = pd.DataFrame(columns= automato.alfabeto, index= [automato.estadoInicial])
+    tabela = pd.DataFrame(columns= automato.alfabeto, index= [automato.estadoInicial])
 
     for letra in automato.alfabeto:
         destino = ''
         for transicao in automato.transicoes:
             if(letra == transicao[1] and transicao[0] == automato.estadoInicial):
                 destino = destino + transicao[2]
-                tabelaAFD.ix[automato.estadoInicial,letra] = destino
+                tabela.ix[automato.estadoInicial,letra] = destino
             else:
-                destino = ''
+                tabela.ix[automato.estadoInicial,letra] = destino
+                destino = destino + ''
+    return tabela
 
+def buscaTransicoes(estado, automato):
+    transicoes = []
+    for letra in automato.alfabeto:
+        destino = ''
+        for transicao in automato.transicoes:
+            for est in range(0,len(estado),2):
+                if(letra == transicao[1] and transicao[0] == estado[est:est+2]):
+                    if(transicao[2] not in destino):
+                        destino = destino + transicao[2]
+                else:
+                    destino = destino + ''
+        transicoes.append(destino)
+    return transicoes
 
-
-    print(tabelaAFD)
+def construcaoNovasLinhas(tabelaInicial, automato, linhaAtual):
+    final = False
+    if(linhaAtual > len(tabelaInicial.index) - 1):
+        final = True
+        return [tabelaInicial, final]
+    for i in automato.alfabeto:
+        if(tabelaInicial._ix[linhaAtual, i] not in tabelaInicial.index):
+            transicoes = buscaTransicoes(tabelaInicial._ix[linhaAtual, i], automato)
+            tabelaInicial.loc[tabelaInicial._ix[linhaAtual, i]] = transicoes
+    return [tabelaInicial, final]
 
 def main():
     automato = Automato()
     carregaDados(automato)
-    montarTabelaAFDInicial(automato)
+    tabelaInicial = montarTabelaAFDInicial(automato)
+    inicio = 0
+    #print(tabelaInicial)
+    retorno = construcaoNovasLinhas(tabelaInicial, automato, inicio)
+    #print(retorno[0])
+    while(retorno[1] == False):
+        inicio += 1
+        retorno = construcaoNovasLinhas(retorno[0], automato, inicio)
+        #print(retorno[0])
+
+    print(retorno[0])
+
+
 
 if __name__ == "__main__":
     main()
